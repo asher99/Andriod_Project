675bbb05eba3a06ddec7e4fe70ab96ab20c9cf36
אשר אלכסנדר <asherb94@gmail.com>
2018-11-07 16:03:28 +0200

    yayyyy we started!!!!

---
+++ app/src/main/java/com/example/myapplication/controller/controller/model/entities/Client.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.controller.controller.model.entities;
+
+public class Client {
+    String name;
+    // user id
+    // bank account
+    // photo
+    // phone number
+    //email
+
+
+
+
+
+
+}
