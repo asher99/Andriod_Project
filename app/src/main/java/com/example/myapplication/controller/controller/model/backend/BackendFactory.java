a868d59abd16b37f50bbd8484fc52c1764b511f0
אשר אלכסנדר <asherb94@gmail.com>
2018-11-12 16:06:18 +0200

    manifest fix

--- app/src/main/java/com/example/myapplication/controller/controller/model/backend/BackendFactory.java
+++ app/src/main/java/com/example/myapplication/controller/controller/model/backend/BackendFactory.java
@@ -8,5 +8,5 @@ public class BackendFactory {
     }
 
     private BackendFactory() {
-    }   
+    }
 }
--- app/src/main/java/com/example/myapplication/controller/controller/model/datasource/Firebase_DBManager.java
+++ app/src/main/java/com/example/myapplication/controller/controller/model/datasource/Firebase_DBManager.java
@@ -1,4 +1,17 @@
 package com.example.myapplication.controller.controller.model.datasource;
 
 public class Firebase_DBManager {
+    public interface Action<T> {
+        void onSuccess(T obj);
+
+        void onFailure(Exception exception);
+
+        void onProgress(String status, double percent);
+    }
+
+    public interface NotifyDataChange<T> {
+        void OnDataChanged(T obj);
+
+        void onFailure(Exception exception);
+    }
 }
