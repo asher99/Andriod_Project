611b94ee501f11edd08d2fa0558db0d4aca11e05
אשר אלכסנדר <asherb94@gmail.com>
2018-11-12 23:03:11 +0200

    style2

--- app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
+++ app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication.controller.controller.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.myapplication.R;
@@ -11,24 +14,65 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity {
 
+    private EditText dest;
+    private EditText phoneNumberField;
+    private EditText emailField;
+    private Button orderButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
-    }
-
-    public void orderRide(View v) {
         //get destination field
-        EditText dest = (EditText)findViewById(R.id.myDestination);
+        dest = (EditText) findViewById(R.id.myDestination);
         String destinationField = dest.getText().toString();
+
         //get email field
-        EditText phoneNumberField = (EditText)findViewById(R.id.myPhoneNumber);
+        phoneNumberField = (EditText) findViewById(R.id.myPhoneNumber);
+
         //get phone number field
-        EditText emailField = (EditText)findViewById(R.id.myEmail);
+        emailField = (EditText) findViewById(R.id.myEmail);
+
+        orderButton = (Button) findViewById(R.id.button2);
+
+        // watches for changes in order to enable the button
+        dest.addTextChangedListener(loginTextWatcher);
+        phoneNumberField.addTextChangedListener(loginTextWatcher);
+        emailField.addTextChangedListener(loginTextWatcher);
+
+
+
     }
+
+    /**
+     * executed at click of the order button
+     * enters the order to the database
+     * @param v
+     */
+    public void orderRide(View v) {
+
+
+    }
+
+    /**
+     * a method that allows to enable order button unless all needed information is entered
+     */
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String userDestInput = dest.getText().toString().trim();
+            String userPhoneInput = phoneNumberField.getText().toString().trim();
+            String userEmailInput = emailField.getText().toString().trim();
+
+            orderButton.setEnabled(!userDestInput.isEmpty() && (!userPhoneInput.isEmpty() || !userEmailInput.isEmpty()));
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    };
 }
