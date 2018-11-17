ff7812479eecc14bc1b7ed08e0f795a230ddc826
David Salem <dsalem9191@gmail.com>
2018-11-17 21:37:36 +0200

    push to pull

--- app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
+++ app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
@@ -9,13 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.myapplication.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends Activity {
 
