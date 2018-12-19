c8e181f8b666c6752938bfc8800bbbfe6708e906
אשר אלכסנדר <asherb94@gmail.com>
2018-12-19 22:38:40 +0200

    loading image

--- app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
+++ app/src/main/java/com/example/myapplication/controller/controller/controller/MainActivity.java
@@ -68,6 +68,9 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
